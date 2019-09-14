2391337c2259a325dcb093e874a862e1ce690e17
Ankit Tyagi <ankittyaagi@gmail.com>
2019-09-14 12:36:18 +0530

    Running

--- Scraper.py
+++ Scraper.py
@@ -20,7 +20,11 @@ def checkprice():
 
     print(cprice)
     print(title.strip())
+
+    if(cprice > 1500):
+        sendmail()
     #print(soup.prettify())
+
 
 def sendmail():
     server = smtplib.SMTP('smtp.gmail.com',587)
@@ -28,10 +32,10 @@ def sendmail():
     server.starttls()
     server.ehlo()
 
-    server.login('ankit7tyagi@gmail.com', hzujvufrqklprfem)
+    server.login('ankit7tyagi@gmail.com', 'hzujvufrqklprfem')
 
     subject = 'Price fell down'
-    cody = 'Check the amazon link https://www.amazon.in/Reebok-Mens-Rush-Running-Shoes/dp/B07FKFXNTH?pf_rd_p=694b9d36-2eb6-4ee4-8d67-51e8321505be&pd_rd_wg=AUmDX&pf_rd_r=FZ74W7SSSB5158P31JW8&ref_=pd_gw_unk&pd_rd_w=Qy7kc&pd_rd_r=0a466d21-02e2-43da-bf61-40f3ce4f16c3&th=1&psc=1'
+    body = 'Check the amazon link https://www.amazon.in/Reebok-Mens-Rush-Running-Shoes/dp/B07FKFXNTH?pf_rd_p=694b9d36-2eb6-4ee4-8d67-51e8321505be&pd_rd_wg=AUmDX&pf_rd_r=FZ74W7SSSB5158P31JW8&ref_=pd_gw_unk&pd_rd_w=Qy7kc&pd_rd_r=0a466d21-02e2-43da-bf61-40f3ce4f16c3&th=1&psc=1'
 
     msg = f"subject: {subject}\n\n{body}"
     server.sendmail(
@@ -40,5 +44,8 @@ def sendmail():
         msg
     )
 
-    Print('Email has been sent  ')
+    print('Email has been sent  ')
     server.quit()
+
+
+checkprice()
